96ca9067db0388f56af3e58e10caa79d643e2bfd
unknown <omerhalely6@gmail.com>
2024-02-27 17:06:41 +0200

    Fixed the models for the encoder and the decoder

--- model.py
+++ model.py
@@ -15,8 +15,8 @@ class Encoder(nn.Module):
         self.var = nn.Linear(hidden_size, latent_size)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv1(self.pool1(x)))
+        x = F.relu(self.conv2(self.pool2(x)))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
 
@@ -44,8 +44,8 @@ class Decoder(nn.Module):
         x = F.relu(self.fc2(x))
         x = torch.reshape(x, (x.size(0), 32, self.height // 4, self.width // 4))
 
-        x = self.up1(F.relu(self.conv1(x)))
-        x = self.up2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv1(self.up1(x)))
+        x = F.relu(self.conv2(self.up2(x)))
         return F.sigmoid(x)
 
 
